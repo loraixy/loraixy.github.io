423c00ac9950fd80a53f280774ebf9d2d00dfef9
loraixy <2656529351@qq.com>
2022-09-17 15:57:01 +0800

    功能完善了点

--- js/index.js
+++ js/index.js
@@ -2,19 +2,44 @@ window.onload = function() {
 
     let mainTitle = document.querySelector('.main-title')
     let headernav = document.querySelector('header')
+    let arrow = document.querySelector('.arrow')
+    arrow.style.opacity = 1
+
     window.onscroll = function(e) {
         var scrollTop = document.documentElement.scrollTop
-        // console.log(scrollTop);
-        // console.log(document.body.scrollTop,  window.screen.height)
-        // console.log(mainTitle.scrollTop)
 
         if (scrollTop > headernav.offsetHeight) {
             headernav.style.display = 'block'
-            console.log(scrollTop, headernav.offsetHeight)
+           
         }
 
         if (scrollTop < headernav.offsetHeight) {
             headernav.style.display = "none"
         }
+
+        if (scrollTop == headernav.offsetHeight) {
+            headernav.style.display = "none"
+            arrow.style.opacity = 1
+            $(".main-title p:nth-child(1)").css({
+                "animation": "move .8s .3s ease-in-out 1 forwards",
+                "display": 'block'
+            })
+            $(".main-title p:nth-child(2)").css({
+                "animation": "move1 .8s .5s ease-in-out 1 forwards",
+                "display": 'block'
+            })
+        } 
+
+        if (scrollTop > mainTitle.offsetHeight) {
+            arrow.style.opacity = 0
+            $(".main-title p:nth-child(1)").css( {
+                "animation": "none",
+                "display": 'none'
+            })
+            $(".main-title p:nth-child(2)").css({
+                "animation": "none",
+                "display": 'none'
+            })
+        } 
     } 
 }

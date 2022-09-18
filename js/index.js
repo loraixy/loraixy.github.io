4e182bd596a6646f1bea40d2de51ddb59ed31192
loraixy <2656529351@qq.com>
2022-09-18 15:04:53 +0800

    今日更新

--- js/index.js
+++ js/index.js
@@ -1,22 +1,23 @@
-window.onload = function() {
+window.onload = function () {
 
     let mainTitle = document.querySelector('.main-title')
     let headernav = document.querySelector('header')
     let arrow = document.querySelector('.arrow')
+    let myText = document.querySelector('.mytext')
     arrow.style.opacity = 1
 
-    window.onscroll = function(e) {
+    window.onscroll = function (e) {
         var scrollTop = document.documentElement.scrollTop
 
         if (scrollTop > headernav.offsetHeight) {
             headernav.style.display = 'block'
-           
+
         }
 
-        if (scrollTop < headernav.offsetHeight) {
+        if (scrollTop < headernav.offsetHeight ) {
             headernav.style.display = "none"
         }
-        
+
         if (!headernav.offsetHeight) {
             // headernav.style.display = "none"
             arrow.style.opacity = 1
@@ -28,11 +29,11 @@ window.onload = function() {
                 "animation": "move1 .8s .5s ease-in-out 1 forwards",
                 "display": 'block'
             })
-        } 
+        }
 
-        if (scrollTop > mainTitle.offsetHeight) {
+        if (scrollTop > mainTitle.offsetHeight  - headernav.offsetHeight) {
             arrow.style.opacity = 0
-            $(".main-title p:nth-child(1)").css( {
+            $(".main-title p:nth-child(1)").css({
                 "animation": "none",
                 "display": 'none'
             })
@@ -40,6 +41,36 @@ window.onload = function() {
                 "animation": "none",
                 "display": 'none'
             })
-        } 
-    } 
+        }
+    }
+
+    let top = 0
+    let timer;
+    arrow.onclick = function () {
+        timer = setInterval(() => {
+            top += 20
+            document.documentElement.scrollTop = top
+            if (document.documentElement.scrollTop >= mainTitle.offsetHeight  - headernav.offsetHeight) {
+                myText.scrollTop = mainTitle.offsetHeight - headernav.offsetHeight
+                top = 0
+                console.log('hello')
+                clearInterval(timer)
+            }
+        }, 4)
+
+    }
+
+    let can = document.querySelector(".canvas-person")
+    let cxt = can.getContext('2d')
+    cxt.beginPath()
+    cxt.moveTo(20, 20)
+    cxt.lineTo(200, 20)
+    cxt.strokeStyle = 'red'
+    cxt.stroke()
+
+    cxt.beginPath()
+    cxt.moveTo(20, 20)
+    cxt.lineTo(100, 50)
+    cxt.strokeStyle = 'blue'
+    cxt.stroke()
 }
